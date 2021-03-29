8800a8846c443e6424c8faa85f46d32d262ea530
Christian <christian@Christians-MacBook-Air.local>
2021-03-29 18:22:12 +0800

    tweaks for frontend

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -57,37 +57,38 @@
             </div>
             <div class="flex-row card-footer d-flex justify-content-between">
                 <div class="text-muted">{{ $post->created_at->diffForHumans() }}</div>
-                <div class="flex-row align-items-center d-flex">
-                    @if (in_array(auth()->user()->id, $post->likes->pluck('user_id')->toArray()))
-                        <form action="{{ route('likes.destroy', $post) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="p-0 btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-arrow-up-square-fill text-primary" viewBox="0 0 16 16">
-                                    <path
-                                        d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
-                                </svg>
-                            </button>
-                        </form>
-                    @else
-                        <form action="{{ route('likes.store', $post) }}" method="POST">
-                            @csrf
-                            <button type="submit" class="p-0 btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="bi bi-arrow-up-square" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
-                                </svg>
-                            </button>
-                        </form>
+                @auth
 
-
-                    @endif
-                    <div class="px-1">
-                        {{ count($post->likes) }}
+                    <div class="flex-row align-items-center d-flex">
+                        @if (in_array(auth()->user()->id, $post->likes->pluck('user_id')->toArray()))
+                            <form action="{{ route('likes.destroy', $post) }}" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="p-0 btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-arrow-up-square-fill text-primary" viewBox="0 0 16 16">
+                                        <path
+                                            d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z" />
+                                    </svg>
+                                </button>
+                            </form>
+                        @else
+                            <form action="{{ route('likes.store', $post) }}" method="POST">
+                                @csrf
+                                <button type="submit" class="p-0 btn">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                        class="bi bi-arrow-up-square" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd"
+                                            d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 9.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z" />
+                                    </svg>
+                                </button>
+                            </form>
+                        @endif
+                        <div class="px-1">
+                            {{ count($post->likes) }}
+                        </div>
                     </div>
-                </div>
+                @endauth
                 <div class="text-muted">
                     @if ($post->comments->isEmpty())
                         Start the discussion!
